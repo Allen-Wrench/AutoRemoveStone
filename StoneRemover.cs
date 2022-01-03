fe77f8cfc18e3fb3a480bf7941c8cdf90cfca0b6
Allen-Wrench <adf.313@gmail.com>
2022-01-02 21:38:14 -0800

    More fix

--- StoneRemover.cs
+++ StoneRemover.cs
@@ -17,10 +17,9 @@ namespace AutoRemoveStone
 		{
 			if (enabled == AutoRemoveStone)
 				return;
-			AutoRemoveStone = enabled;
-			if (AutoRemoveStone && !TaskRunning && MySession.Static.ControlledEntity is MyCubeBlock)
+			if (!AutoRemoveStone && MySession.Static.ControlledEntity is MyCubeBlock)
 			{
-				TaskRunning = true;
+				AutoRemoveStone = enabled;
 				MySession.Static.AddUpdateCallback(new MyUpdateCallback(new Func<bool>(Update)));
 				return;
 			}
@@ -30,7 +29,6 @@ namespace AutoRemoveStone
 		{
 			if (!AutoRemoveStone)
 			{
-				TaskRunning = false;
 				return true;
 			}
 			if (Counter < 500)
@@ -41,21 +39,18 @@ namespace AutoRemoveStone
 			Counter = 0;
 			if (MySession.Static.ControlledEntity is MyCubeBlock)
 			{
-				if (MySession.Static.ControlledEntity is MyCubeBlock)
+				HashSet<MyCubeBlock> inventories = (MySession.Static.ControlledEntity as MyCubeBlock).CubeGrid.Inventories;
+				for (int i = 0; i < inventories.Count; i++)
 				{
-					HashSet<MyCubeBlock> inventories = (MySession.Static.ControlledEntity as MyCubeBlock).CubeGrid.Inventories;
-					for (int i = 0; i < inventories.Count; i++)
+					MyInventory inventory = inventories.ElementAt(i).GetInventory(0);
+					List<MyPhysicalInventoryItem> items = inventory.GetItems();
+					if (items.Count != 0)
 					{
-						MyInventory inventory = inventories.ElementAt(i).GetInventory(0);
-						List<MyPhysicalInventoryItem> items = inventory.GetItems();
-						if (items.Count != 0)
+						for (int j = 0; j < items.Count; j++)
 						{
-							for (int j = 0; j < items.Count; j++)
+							if (items[j].Content.SubtypeName.Contains("Stone"))
 							{
-								if (items[j].Content.SubtypeName.Contains("Stone"))
-								{
-									inventory.RemoveItemsAt(inventory.GetItemIndexById(items[j].ItemId), new MyFixedPoint?(items[j].Amount), true, false, null);
-								}
+								inventory.RemoveItemsAt(inventory.GetItemIndexById(items[j].ItemId), new MyFixedPoint?(items[j].Amount), true, false, null);
 							}
 						}
 					}
@@ -63,12 +58,9 @@ namespace AutoRemoveStone
 				return false;
 			}
 			AutoRemoveStone = false;
-			TaskRunning = false;
 			return true;
 		}
 		public static bool AutoRemoveStone { get; private set; }
-
-		public static bool TaskRunning { get; private set; }
 
 		public static int Counter { get; private set; }
 	}
